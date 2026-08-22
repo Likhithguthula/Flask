c5879b6307580df53b9ad83dbe4b0fc3ed8f9cdc
LIKHITH <125430200+Likhithguthula@users.noreply.github.com>
2026-08-22 21:08:55 +0530

    Day - 4

--- app_react.py
+++ app_react.py
@@ -75,6 +75,74 @@ if __name__ == "__main__":
     app_react.run(debug=True)
 '''
 
+'''
+from flask import Flask, request
+app_react = Flask(__name__)
+@app_react.route('/')
+
+def home():
+    return f"Method {request.method}"
+if __name__ == "__main__":
+    app_react.run(debug=True)
+'''
+
+
+'''
+from flask import Flask, request
+app_react = Flask(__name__)
+@app_react.route('/student')
+
+def home():
+    name = request.args.get("name")
+    age = request.args.get("age")
+    batch = request.args.get("batch")
+
+    return f"Name: {name}, Age:{age}, batch:{batch}"
+if __name__ == "__main__":
+    app_react.run(debug=True)
+'''
+
+
+'''
+from flask import Flask, request
+app_react = Flask(__name__)
+@app_react.route('/login', methods = ['GET', 'POST'])
+
+def login():
+    if request.method == "POST":
+       email = request.form.get("email")
+       password = request.form.get("password")
+       return f"email: {email}, password: {password}"
+   
+    return """
+    <form method = "POST">
+        <input type = "email" name = "email">
+        <input type = "password" name = "password">
+        <button type = "submit">Login</button>
+    </form>
+    """
+if __name__ == "__main__":
+    app_react.run(debug=True)
+'''
+
+
+'''
+from flask import Flask, request
+app_react = Flask(__name__)
+@app_react.route('/student', methods = ['GET', 'POST'])
+
+def student():
+    if request.method == "GET":
+        return "Please send student data using POST"
+    data = request.get_json()
+    name = request.get_json("name")
+    age = request.get_json("age")
+    return f"Name: {name}, Age:{age}"
+
+if __name__ == "__main__":
+    app_react.run(debug=True)
+'''
+
 
 
 
